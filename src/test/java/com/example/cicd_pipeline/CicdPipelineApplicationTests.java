9eb4be3aee3b39651af50e0af57aab9ab9d0efc6
viduthranaweera2001 <viduthranaweera1234@gmail.com>
2025-11-21 20:43:22 +0530

    first

---
+++ src/test/java/com/example/cicd_pipeline/CicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
